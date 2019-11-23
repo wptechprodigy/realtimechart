566b3bbff8ae5faa1f7637f7351253f8f03bcbb5
wptechprodigy <waheed.freelancer16@gmail.com>
2019-11-23 11:46:42 +0100

    Setup apollo-client for client

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a WebSocket link:
+const link = new WebSocketLink({
+  uri: `ws://localhost:8080/v1alpha1/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        'x-hasana-admin-secret': 'mylongsecretkey'
+      }
+    }
+  }
+});
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
